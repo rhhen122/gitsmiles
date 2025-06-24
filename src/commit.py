73cee99e1c0632cf2719c148f3e2bb4628a105cd
rhhen122 <rokyhenderson222@gmail.com>
2025-06-24 14:25:45 +1200

    So i edited COMMIT ig???

--- src/commit.py
+++ src/commit.py
@@ -1,16 +1,20 @@
 import os
+
+
 # This defines an easier command to type
 def sh(cmd):
     os.system(cmd)
+
+
 while True:
-    cnt = input('Files to commit, end with ! : ')
-    if cnt == '!':
+    cnt = input("Files to commit, end with ! : ")
+    if cnt == "!":
         break
     else:
-        sh(f'git add {cnt}')
+        sh(f"git add {cnt}")
 while True:
-    cnt = input('Commit Message, if none end with ! : ')
-    if cnt == '!':
+    cnt = input("Commit Message, if none end with ! : ")
+    if cnt == "!":
         break
     else:
         sh(f'git commit -m "{cnt}"')
