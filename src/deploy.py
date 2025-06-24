905b0f4c27bbc53375e3aec2cf385222ffeccadf
rhhen122 <rokyhenderson222@gmail.com>
2025-06-24 14:43:06 +1200

    Made DEPLOY better

--- src/deploy.py
+++ src/deploy.py
@@ -1,14 +1,18 @@
 import os
+
+
 def sh(cmd):
     os.system(cmd)
+
+
 while True:
-    branch = input('Branch name: ')
-    cnt = input('1. Tags, 2. Commits: ')
-    if cnt == '2':
-        cnt = input('Enter Deploy location for git: ')
-        sh(f'git push {cnt} {branch}')
+    cnt = input("1. Tags, 2. Commits: ")
+    if cnt == "2":
+        branch = input("Branch name: ")
+        cnt = input("Enter Deploy location for git: ")
+        sh(f"git push {cnt} {branch}")
         break
     else:
-        cnt = input('Enter Deploy location for git: ')
-        sh(f'git push {cnt} --tags')
+        cnt = input("Enter Deploy location for git: ")
+        sh(f"git push {cnt} --tags")
         break
