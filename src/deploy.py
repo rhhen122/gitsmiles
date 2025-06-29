f54b191340c744231e188955bb3567db35638bf3
rhhen122 <rokyhenderson222@gmail.com>
2025-06-29 13:31:59 +1200

    Updated DEPLOY

--- src/deploy.py
+++ src/deploy.py
@@ -6,13 +6,18 @@ def sh(cmd):
 
 
 while True:
-    cnt = input("1. Tags, 2. Commits: ")
+    cnt = input("1. Tags, 2. Commits, 3. Both: ")
     if cnt == "2":
         branch = input("Branch name: ")
         cnt = input("Enter Deploy location for git: ")
         sh(f"git push {cnt} {branch}")
         break
-    else:
+    elif cnt == "1":
         cnt = input("Enter Deploy location for git: ")
         sh(f"git push {cnt} --tags")
         break
+    elif cnt == "3":
+        branch = input("Branch name: ")
+        cnt = input("Enter Deploy location for git: ")
+        sh(f"git push {cnt} {branch}")
+        sh(f"git push {cnt} --tags")
